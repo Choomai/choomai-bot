8193d3d3b3d84c003e4bb5e4757c662b87071aaa
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-12-17 21:04:00 +0700

    Add category option to vc "new" subcommand

--- commands/vc.js
+++ commands/vc.js
@@ -12,11 +12,13 @@ async function execute(interaction, options) {
             await interaction.deferReply({ flags: visibility ? MessageFlags.Ephemeral : undefined });
 
             targetUser = interaction.options.getUser("user");
-            if (!category) return interaction.editReply("Could not find Voice Channels category!");
+            targetCategory = interaction.options.getChannel("category");
+            if (targetCategory && targetCategory.type !== ChannelType.GuildCategory) return interaction.editReply({ message: "Invalid channel category!", flags: MessageFlags.Ephemeral });
+
             const channel = await interaction.guild.channels.create({
                 name: `${interaction.user.username}'s VC`,
                 type: ChannelType.GuildVoice,
-                parent: undefined, // VC category ID missing
+                parent: targetCategory ?? undefined,
                 permissionOverwrites: [
                     {
                         id: interaction.guild.id,
@@ -84,6 +86,11 @@ module.exports = {
                 .setName("user")
                 .setDescription("Set the user that are allow to join the private VC")
                 .setRequired(true)
+            )
+            .addChannelOption(option => option
+                .setName("category")
+                .setDescription("What category to put the VC in")
+                .setRequired(false)
             )
             .addBooleanOption(option => option
                 .setName("visible")
