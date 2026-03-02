189970c61b2792934a42e172dedaf13209b178b3
Choomai <nguyencaonguyen0944322545@gmail.com>
2026-03-03 02:28:46 +0700

    feat(status): add system status
    CPU Load & Temp, RAM, Disks status (not yet), Network RX & TX, Docker
    Because of Discord not allowing a field value to be too long, we will send the disks data using a follow-up response.

--- commands/status.js
+++ commands/status.js
@@ -1,6 +1,39 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { exec } = require("node:child_process");
+const SysInfo = require("systeminformation")
 const net = require("node:net");
+
+async function getSystemStatus() {
+    // TODO: Format all bytes value to human-readable units
+    // TODO: Put disks status into a follow-up response
+    try {
+        const sysInfoData = await Promise.all([
+            SysInfo.currentLoad(),  
+            SysInfo.cpuTemperature(),
+            SysInfo.mem(),
+            SysInfo.diskLayout(),
+            SysInfo.networkStats(),
+            SysInfo.dockerInfo()
+        ])
+
+        // const disksStatus = [];
+        // sysInfoData[3].forEach(disk => {
+        //     disksStatus.push({ device, type, name, size, smartStatus, temperature } = disk)
+        // })
+
+        return [
+            { name: "CPU Load", value: `Avg: \`${sysInfoData[0].avgLoad}\` / Current: \`${sysInfoData[0].currentLoad.toFixed(2)}\`` },
+            { name: "CPU Temp", value: sysInfoData[1].main.toString(), inline: true },
+            { name: "RAM", value: `Used: \`${sysInfoData[2].used}\` / Total: \`${sysInfoData[2].total}\`` },
+            // { name: "Disk", value: `\`\`\`json${JSON.stringify(disksStatus, null, 2)}\`\`\`` },
+            { name: "Network", value: `RX: \`${sysInfoData[4][0].rx_bytes}\` / TX: \`${sysInfoData[4][0].tx_bytes}\`` },
+            { name: "Docker", value: `Running: ${sysInfoData[5].containersRunning} / Total: ${sysInfoData[5].containers}` }
+        ]
+    } catch (err) {
+        console.error(err);
+        return [ { name: "System Status", value: "Error!" } ]
+    }
+}
 
 async function execute(interaction) {
     await interaction.deferReply();
@@ -9,9 +42,10 @@ async function execute(interaction) {
         .setAuthor({ name: "Server Status", iconURL: "https://assets.choomai.net/icons/network_256.png", url: "https://l.choomai.net/status" })
         .setTimestamp()
         .addFields(
-            {name: "Website", value: "🕒Checking", inline: true},
-            {name: "VPN", value: "🕒Checking", inline: true},
-            {name: "Minecraft", value: "🕒Checking", inline: true}
+            { name: "Website", value: "🕒Checking", inline: true },
+            { name: "VPN", value: "🕒Checking", inline: true },
+            { name: "Minecraft", value: "🕒Checking", inline: true },
+            ...(await getSystemStatus())
         );
     await interaction.editReply({embeds: [embedReply]});
 
