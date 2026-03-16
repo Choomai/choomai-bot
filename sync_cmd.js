6ed6254e4677dd8a16b30be37d954f4cb62380a7
Choomai <nguyencaonguyen0944322545@gmail.com>
2026-03-17 00:00:05 +0700

    refactor: correct the env name from DCG to SERVER
    migrating those for multi-servers use will be the pain in the ass, noticing it just now.
    ehh I dockerized it anyway, might as well make it multi-servers (in the future ofc).

--- sync_cmd.js
+++ sync_cmd.js
@@ -22,7 +22,7 @@ const rest = new REST().setToken(process.env.TOKEN);
 
 	// The put method is used to fully refresh all commands in the guild with the current set
 	const data = await rest.put(
-		Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DCG_ID),
+		Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.SERVER_ID),
 		{ body: commands },
 	);
 
