a7b6a850d897ef6491ae713ebf00cb510112d044
Choomai <nguyencaonguyen0944322545@gmail.com>
2026-03-25 01:56:06 +0700

    refactor(dcall): use command invoker instead of specifying an user

--- commands/dcall.js
+++ commands/dcall.js
@@ -9,16 +9,14 @@ async function execute(interaction) {
         if (!interaction.member.permissions.has(PermissionFlagsBits.MoveMembers))
             return await interaction.reply("You do not have permission to use this command.");
 
-        const user = await interaction.guild.members.fetch(process.env.CHOOMAI);
-        channel = user.voice.channel;
-        if (!channel) return await interaction.reply("The bot owner (or the specified user) is not in a voice channel.");
+        channel = interaction.member.voice.channel;
+        if (!channel) return await interaction.reply("You are not in a voice channel.");
     } else if (interaction instanceof CommandInteraction) {
         if (!interaction.memberPermissions.has(PermissionFlagsBits.MoveMembers))
             return await interaction.reply({ content: "You do not have permission to use this command.", flags: MessageFlags.Ephemeral });
 
-        const user = interaction.options.getUser("user") || await interaction.guild.members.fetch(process.env.CHOOMAI);
-        channel = user.voice.channel;
-        if (!channel) return await interaction.reply({ content: "The bot owner (or the specified user) is not in a voice channel.", flags: MessageFlags.Ephemeral });
+        channel = interaction.member.voice.channel;
+        if (!channel) return await interaction.reply({ content: "You are not in a voice channel.", flags: MessageFlags.Ephemeral });
     }
 
     interaction.reply(`Starting to disconnect everyone from ${channel}.`);
@@ -33,12 +31,7 @@ module.exports = {
     messageCommand: true,
     data: new SlashCommandBuilder()
         .setName("dcall")
-        .setDescription("Disconnect everyone from the voice channel that the bot owner is in")
-        .addUserOption(option => option
-            .setName("user")
-            .setDescription("The user that are in the target VC. Default to the bot owner")
-            .setRequired(false)
-        )
+        .setDescription("Disconnect everyone from the voice channel that the invoker is in")
         .setDefaultMemberPermissions(PermissionFlagsBits.MoveMembers),
     execute
 }
