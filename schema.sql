55b13987fd926d20b90ce2c2c588fba60095e23a
Choomai <nguyencaonguyen0944322545@gmail.com>
2026-03-27 23:30:46 +0700

    chore(db); remove unnesscary table

--- schema.sql
+++ schema.sql
@@ -1,11 +1,3 @@
-CREATE TABLE IF NOT EXISTS `afk_list` (
-    `user_id` varchar(255) NOT NULL,
-    `end_time` bigint NOT NULL,
-    `afk_time` bigint NOT NULL,
-    `username` varchar(255) NOT NULL,
-    PRIMARY KEY (`user_id`)
-);
-
 CREATE TABLE IF NOT EXISTS `log_channels` (
     `guild_id` varchar(32) NOT NULL,
     `channel_id` varchar(32) NOT NULL,
