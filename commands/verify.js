1ad318f951bdb663f7c7a0354e96f42717f4e42a
Choomai <nguyencaonguyen0944322545@gmail.com>
2026-03-28 22:57:11 +0700

    feat(verify): use Turnstile verification, the command is ready to use

--- commands/verify.js
+++ commands/verify.js
@@ -1,72 +1,29 @@
-const { SlashCommandBuilder, MessageFlags } = require("discord.js");
+const { SlashCommandBuilder, CommandInteraction, MessageFlags, PermissionFlagsBits } = require("discord.js");
+const crypto = require("node:crypto");
+const { formatTime } = require("../include/time.js");
 
-async function execute(interaction, verifyAttempts) {
-    return await interaction.reply({ content: "Command disabled!", flags: MessageFlags.Ephemeral });
+/**
+ * @param {CommandInteraction} interaction 
+ */
+async function execute(interaction) {
+    if (interaction.member.roles.cache.has(process.env.SERVER_MEMBER_ID) || interaction.memberPermissions.has(PermissionFlagsBits.Administrator))
+        return void interaction.reply({ content: "You are already verified.", flags: MessageFlags.Ephemeral });
 
-    const guildMember = interaction.guild.members.cache.get(interaction.user.id);
-
-    if (guildMember.roles.cache.has(process.env.SERVER_MEMBER_ID))
-        return await interaction.reply({ content: "You have already been verified!", flags: MessageFlags.Ephemeral });
-
-    if (verifyAttempts[interaction.user.id] >= 3)
-        return await interaction.reply({ content: "You have reached the maximum number of attempts. Please try again in 6 hours.", flags: MessageFlags.Ephemeral });
-
-    let num1, num2, problem, answer, operation = Math.floor(Math.random() * 4);
-    switch(operation) {
-        case 0: // addition
-            num1 = Math.floor(Math.random() * 101);
-            num2 = Math.floor(Math.random() * 101);
-            problem = `${num1} + ${num2}`;
-            answer = num1 + num2;
-            break;
-        case 1: // subtraction
-            num1 = Math.floor(Math.random() * 101);
-            num2 = Math.floor(Math.random() * num1);
-            problem = `${num1} - ${num2}`;
-            answer = num1 - num2;
-            break;
-        case 2: // multiplication
-            num1 = Math.floor(Math.random() * 10);
-            num2 = Math.floor(Math.random() * 10);
-            problem = `${num1} * ${num2}`;
-            answer = num1 * num2;
-            break;
-        case 3: // division
-            num2 = Math.floor(Math.random() * 10) + 1;
-            num1 = num2 * (Math.floor(Math.random() * 10) + 1);
-            problem = `${num1} / ${num2}`;
-            answer = num1 / num2;
-            break;
+    const attempts = await interaction.client.redis.get(`choomai_bot:verify:${interaction.user.id}:attempts`);
+    if (attempts && parseInt(attempts) >= 3) {
+        const ttlLeft = await interaction.client.redis.ttl(`choomai_bot:verify:${interaction.user.id}:attempts`);
+        return void interaction.reply({
+            content: `You have reached the maximum verification attempts. Please try again in ${formatTime(ttlLeft)}.`,
+            flags: MessageFlags.Ephemeral
+        });
     };
 
-    const dmChannel = await interaction.user.createDM();
-    const collector = dmChannel.createMessageCollector({
-        filter: msg => msg.author.id == interaction.user.id, 
-        time: 30000
-    });
+    await interaction.client.redis.incr(`choomai_bot:verify:${interaction.user.id}:attempts`);
 
-    await interaction.reply({ content: "Check your DM.", flags: MessageFlags.Ephemeral });
-    await interaction.user.send(`You have 30s to solve this problem: ${problem}`);
-    
-    collector.on("collect", msg => {
-        if (parseInt(msg.content) == answer) {
-            guildMember.roles.add(process.env.SERVER_MEMBER_ID);
-            guildMember.roles.remove(process.env.SERVER_INACTIVE_ID);
-            interaction.user.send("You have been verified!");
-            return collector.stop();
-        } else {
-            if (!verifyAttempts[interaction.user.id]) verifyAttempts[interaction.user.id] = 0;
-            if (++verifyAttempts[interaction.user.id] >= 3) return collector.stop();
-            return interaction.user.send("Incorrect answer. Please try again.");
-        }
-    });
-
-    collector.on("end", collected => {
-        if (collected.size == 0 || verifyAttempts[interaction.user.id] >= 3) {
-            interaction.user.send("Time is up! You failed.\nYou have reached the maximum number of attempts. Please try again in 6 hours.");
-            setTimeout(() => {delete verifyAttempts[interaction.user.id]}, 21600000);
-        };
-    });
+    const uuid = crypto.randomUUID();
+    await interaction.client.redis.setex(`choomai_bot:verify:${uuid}`, 10 * 60, interaction.user.id);
+    await interaction.user.send(`Your verification URL is: https://discord.choomai.net/verify/${uuid}`);
+    interaction.reply({ content: "Check your DM for verification URL.", flags: MessageFlags.Ephemeral });
 }
 
 module.exports = {
