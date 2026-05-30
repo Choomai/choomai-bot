6096076ac673d9f8fc5371bea2cc62f4c7fadc2b
Choomai <nguyencaonguyen0944322545@gmail.com>
2026-05-30 16:36:46 +0700

    refactor(sync): migrate to ESM (i forgot)

--- sync_cmd.js
+++ sync_cmd.js
@@ -1,23 +1,17 @@
-const { REST, Routes } = require('discord.js');
-require("dotenv").config();
-const fs = require("node:fs");
-const path = require("node:path");
-
-const commands = [];
-const commandsPath = path.join(__dirname, "commands/");
-const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-for (const file of commandsFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	if ('data' in command && 'execute' in command) {
-		commands.push(command.data.toJSON());
-	} else {
-		console.warn(`The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-}
+import { REST, Routes } from "discord.js";
+import "./include/config.js";
+import loadCommands from "./commands/commands.js";
+import fs from "node:fs";
+import path from "node:path";
 
 const rest = new REST().setToken(process.env.TOKEN);
 (async () => {
+	const commands = [];
+    const loadedCommands = await loadCommands();
+	for (const cmd of loadedCommands) {
+		commands.push(cmd.data.toJSON());
+		console.log(`Loaded command: ${cmd.data.name}`);
+	}
 	console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 	// The put method is used to fully refresh all commands in the guild with the current set
